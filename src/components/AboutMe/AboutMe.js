715a3a2ca02cf3f3ad366f121b9f7e8bd68d4fb5
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-08-27 15:39:22 -0400

    make changes to AboutMe bio

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -20,10 +20,10 @@ const AboutMe = () => {
             My name is Uriel Rodriguez and I'm a software developer based out of New York City. I recently completed the Full Stack Web Development bootcamp program at Flatiron School. There I worked on several projects using various technologies including, but not limited to, Ruby on Rails, JavaScript and React.
           </p>
           <p>
-            My journey to becoming a full stack web developer started a year ago when I decided to transition from the social services field to seek change and new opportunities in my career and life. With that change, I took with me my curiosity for how things work, whether it was the psychology of human behavior or concepts like abstraction and how it helps programs scale as they grow in complexity.
+            My journey to becoming a full stack web developer started 6 months ago when I decided to transition from the social services field to seek change and new opportunities in my career and life. With that change, my curiosity for how things work carried over into concepts like abstraction and how it helps programs scale as they grow in complexity.
           </p>
           <p>
-            While I'm mostly knowledgeable in front-end development, I'm continuously adding new skill sets, including back-end related technologies such as Node.js as I hope to work and focus on data organization and application architecture and scalability. Aside from programming, I'm learning more on programming but also enjoy being outdoors in nature, whether hiking or running.
+            While most of my knowledge is in front-end development, I'm continuously adding new skill sets. Currently I'm focused on back-end related technologies such as Node.js as I hope to work on data organization and application architecture and scalability. Aside from programming, I like to balance my time by enjoying the outdoors whether it's hiking or running and spending time with family.
           </p>
         </div>
       </div>
