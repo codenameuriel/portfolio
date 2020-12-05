2ecc14a74f516a901ca00d94295f08c7ccb3747c
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-12-05 14:32:53 -0500

    restyle Navbar and create DrawerToggle component

---
+++ src/components/Navigation/DrawerToggle/DrawerToggle.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import DrawerToggleStyles from './DrawerToggle.module.css';
+
+const DrawerToggle = props => {
+  return (
+    <div 
+      className={DrawerToggleStyles.DrawerToggle}>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default DrawerToggle;
--- src/components/Navigation/Navbar/Navbar.js
+++ src/components/Navigation/Navbar/Navbar.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import NavbarStyles from './Navbar.module.css';
 import NavItems from '../NavItems/NavItems';
+import DrawerToggle from '../DrawerToggle/DrawerToggle';
 
 const Navbar = () => {
   return (
-    <header className={NavbarStyles.Navbar}>
+    <header className={NavbarStyles.Header}>
       <div className={NavbarStyles.Logo}>
         {/* Logo goes here */}
       </div>
-      <nav>
+      <nav className={NavbarStyles.Nav}>
+        <DrawerToggle />
         <NavItems />
       </nav>
     </header>
